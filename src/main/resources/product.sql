7feea7ab069349c2793a696484c67508232193e6
kissloveheart <nguyenhiep581@gmail.com>
2022-02-09 16:06:37 +0700

    repository

---
+++ src/main/resources/product.sql
@@ -0,0 +1,12 @@
+
+INSERT INTO "PUBLIC"."CATEGORY" VALUES
+(1, 'Phone'),
+(2, 'Laptop');
+
+INSERT INTO "PUBLIC"."PRODUCT" VALUES
+(1, NULL, 'Iphone 8', 399.0, 1),
+(2, NULL, 'Iphone 9', 499.0, 1),
+(3, NULL, 'Iphone 10', 599.0, 1),
+(4, NULL, 'Iphone 11', 699.0, 1),
+(5, NULL, 'Dell X99', 1999.0, 2),
+(6, NULL, 'Lenovo Y11', 999.0, 2);
